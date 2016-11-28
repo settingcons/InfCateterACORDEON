af7d5c052939dc5a704ca5e4c40fdd98eecfa051
settingcons <settingcons@gmail.com>
2016-11-28 11:56:07 +0100

    vAsset
    Amb asset connectat a http i pdf copiats

--- www/js/paginas/pageMENU.js
+++ www/js/paginas/pageMENU.js
@@ -19,8 +19,8 @@ function paginaInfoPDF(idioma,fichero){
         else {
             ruta = "file:///sdcard/infCateter/" + idioma + "/";
             fic = ruta + fichero;
-            //window.plugins.fileOpener.open(fic);
-            cordova.plugins.fileOpener2.open(fic, 'application/pdf');
+            window.plugins.fileOpener.open(fic); //descomentat 28/11/2016
+            //cordova.plugins.fileOpener2.open(fic, 'application/pdf'); //comentat 28/11/2016
         }
     }
     catch(ex)
